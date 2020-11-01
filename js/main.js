674f377913e28c59d015d24f03d750bd35eb23cb
Aman Singh <am8400175130@gmail.com>
2020-11-01 15:22:17 +0530

    Improving the Responsiveness

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const responsive ={
+    0:{
+        items:1
+    },
     320:{
         items:1
-    }
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    },
 }
 
 $(document).ready(function(){
